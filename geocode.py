4dcfbb560670a4975f1c0da4ce01539c7fd9b5bf
Brent Barrese <brent.barrese@gmail.com>
2021-06-22 10:33:37 -0700

    update on combining functions

--- geocode.py
+++ geocode.py
@@ -1,30 +1,32 @@
 from numpy import NaN, add
 import pandas as pd
 from pandas.core.dtypes.missing import notnull
+from pandas.core.indexes import base
 import requests
 import os
+import gmplot
 
 # ****************************************** Constants ******************************************
 # Google API Key used for Maps API - Can use different key here if you have one
 API_KEY = os.environ.get('MAPS_API_KEY')
 #API_KEY = 'put own key here'
 # Name of output file and its path
-output_file = ''
+OUTPUT_FILE = 'testOutput.csv'
 # Name of input file and its path, '.' is the current working directory
-input_file = "./data-raw.csv"
-#input_file = "./data-raw-excel.xlsx"
+INPUT_FILE = "./data-raw.csv"
+#INPUT_FILE = "./data-raw-excel.xlsx"
 
 # ******************************** Column Names - Edit as needed ********************************
-# Column name in input_file that contains the full address
-address_column = ("ADDRESSSTREET")
+# Column name in INPUT_FILE that contains the full address
+ADDRESS_COLUMN = ("ADDRESSSTREET")
 # City
-city_column = ("City Place")
+CITY_COLUMN = ("City Place")
 # If no address, will have cross street to use
-cross_street_column = ("CROSSSTREET")
+CROSS_STREET_COLUMN = ("CROSSSTREET")
 
 # ************************************ Function Definitions **************************************
 # Function to get results from google
-def geocode(address):
+def geocode(address, complete_response_string=False):
     print('Geocoding...')
 
     params = {
@@ -35,7 +37,9 @@ def geocode(address):
     base_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
     #creates request call and converts to json format
-    response = requests.get(base_url, params=params).json()
+    #response = requests.get(base_url, params=params).json()
+    results = requests.get(base_url, params=params)
+    response = results.json()
     response.keys()
     print(response)
 
@@ -45,7 +49,27 @@ def geocode(address):
         lon = geometry['location']['lng']
 
     print(lat,lon)
-    return response
+    print(geometry)
+
+    answer = response['results'][0]
+    output = {
+        "formatted_address" : answer.get('formatted_address'),
+        "latitude": answer.get('geometry').get('location').get('lat'),
+        "longitude": answer.get('geometry').get('location').get('lng'),
+        "accuracy": answer.get('geometry').get('location_type'),
+        "google_place_id": answer.get("place_id"),
+        "type": ",".join(answer.get('types')),
+        "postcode": ",".join([x['long_name'] for x in answer.get('address_components') 
+                                if 'postal_code' in x.get('types')])
+}
+
+    output['input_string'] = address
+    output['number_of_results'] = len(response['results'])
+    output['status'] = response.get('status')
+    if complete_response_string:
+        output['response'] = response
+
+    return output
 
 # Function to parse data and get addresses to put in list
 # Returns list of addresses
@@ -54,36 +78,52 @@ def getAddresses():
 
 
 # Function to write to map
-def plotResults():
+def plotResults(results):
     print('Plotting Results...')
+
+    #center map and zoom level
+    gmap = gmplot.GoogleMapPlotter(47.608013,-122.335167, 10, apikey=API_KEY)
+
+    for result in results:
+        print('this is a result in the result list!')
+        gmap.coloricon = "http://www.googlemapsmarkers.com/v1/%s/"
+        lat = result['latitude']
+        lon = result['longitude']
+        print(lat,lon)
+
+        #draw circle around marker, radius in meters
+        #200 feet = 60.96 meters
+        gmap.circle(48.1013714, -122.2271894, 60.96)
+
+    #where to draw map (. = current directory)
+    gmap.draw(".\map.html")
 
 
 # ************************************** Main Function ******************************************
 def main():
     print('This is Main...')
-    df = pd.read_csv(input_file)
+    df = pd.read_csv(INPUT_FILE)
     #print(df)
 
     # Handle where address column is null, so cross street location
-    bool_series1 = pd.notnull(df[cross_street_column])
+    bool_series1 = pd.notnull(df[CROSS_STREET_COLUMN])
     #print(df[bool_series])
     df_crossStreet = df[bool_series1].copy()
     #format for list --> 'address, city'
-    crossStreetAddresses = (df_crossStreet[cross_street_column] + ',' + df_crossStreet[city_column]).tolist()
+    crossStreetAddresses = (df_crossStreet[CROSS_STREET_COLUMN] + ',' + df_crossStreet[CITY_COLUMN]).tolist()
 
     #Handle where address column not null, so address location
-    bool_series2 = pd.notnull(df[address_column])
+    bool_series2 = pd.notnull(df[ADDRESS_COLUMN])
     df_addresses = df[bool_series2].copy()
     #format for list --> 'address, city'
-    addresses = (df_addresses[address_column] + ',' + df_addresses[city_column]).tolist()
+    addresses = (df_addresses[ADDRESS_COLUMN] + ',' + df_addresses[CITY_COLUMN]).tolist()
 
     print('\n\n Combining Lists Now... \n\n')
 
     combinedLocations = crossStreetAddresses + addresses
+    
     for location in combinedLocations:
         print(location)
-
-    # At this point all locations needing to be geocoded and mapped are in combinedLocations [] *****************************
 
     # List for storing results
     geocodedResults = []
@@ -91,8 +131,11 @@ def main():
     for location in combinedLocations:
         geocodedResults.append(geocode(location))
 
-    for result in geocodedResults:
-        print('\n', result,'\n')
+    # Plot all found locations to a map
+    plotResults(geocodedResults)
+
+    # Write to csv
+    pd.DataFrame(geocodedResults).to_csv(OUTPUT_FILE, encoding='utf8')
 
 if __name__ == "__main__":
     main()
--- test-google-api-geocoding.py
+++ test-google-api-geocoding.py
@@ -14,7 +14,9 @@ params = {
 base_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
 #creates request call and converts to json format
-response = requests.get(base_url, params=params).json()
+#response = requests.get(base_url, params=params).json()
+results = requests.get(base_url, params=params)
+response = results.json()
 response.keys()
 print(response)
 
@@ -24,6 +26,27 @@ if response['status'] == "OK":
     lon = geometry['location']['lng']
 
 print(lat,lon)
+print(geometry)
+
+answer = response['results'][0]
+output = {
+    "formatted_address" : answer.get('formatted_address'),
+    "latitude": answer.get('geometry').get('location').get('lat'),
+    "longitude": answer.get('geometry').get('location').get('lng'),
+    "accuracy": answer.get('geometry').get('location_type'),
+    "google_place_id": answer.get("place_id"),
+    "type": ",".join(answer.get('types')),
+    "postcode": ",".join([x['long_name'] for x in answer.get('address_components') 
+                            if 'postal_code' in x.get('types')])
+}
+
+output['input_string'] = address
+output['number_of_results'] = len(response['results'])
+output['status'] = response.get('status')
+
+output['response'] = response
+
+print(output)
 
 
 # def get_google_results(address, api):
